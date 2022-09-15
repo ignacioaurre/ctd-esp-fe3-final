c7253ed046f9aeb1463b312af4b714a419e0aedc
ignacioaurrecoechea <ignacio.aurrecoechea@mercadolibre.com>
2022-09-14 23:56:10 -0300

    Converted offset to number in api Comics

--- pages/api/comics.route.ts
+++ pages/api/comics.route.ts
@@ -4,7 +4,8 @@ import { getComics } from 'dh-marvel/services/marvel/marvel.service';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Comic[]>){
     const { offset } = req.query;
+    const offset2 = offset ? parseInt(offset.toString()) : 0;
     let limit = 12;
-    const response = await getComics(offset, limit)
+    const response = await getComics(offset2, limit)
     res.status(200).json(response.data.results); 
 }
