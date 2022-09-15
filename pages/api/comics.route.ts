c03f61f42e33599dd59ca2e25fa31568d46ac23d
ignacioaurrecoechea <ignacio.aurrecoechea@mercadolibre.com>
2022-09-15 00:29:31 -0300

    Fixed parseInt method and changed variable name

--- pages/api/comics.route.ts
+++ pages/api/comics.route.ts
@@ -4,8 +4,8 @@ import { getComics } from 'dh-marvel/services/marvel/marvel.service';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Comic[]>){
     const { offset } = req.query;
-    const offset2 = offset ? parseInt(offset.toString()) : 0;
+    const offsetNumber = offset ? parseInt(offset.toString(), 10) : 0;
     let limit = 12;
-    const response = await getComics(offset2, limit)
+    const response = await getComics(offsetNumber, limit)
     res.status(200).json(response.data.results); 
 }
