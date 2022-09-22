72c4c45ac7811a34ed2c2ea539323b9cdd482d4c
ignacioaurrecoechea <ignacio.aurrecoechea@mercadolibre.com>
2022-09-21 23:26:35 -0300

    Corrected tpyes

--- context/reducer.ts
+++ context/reducer.ts
@@ -1,5 +1,6 @@
+import { OrderState } from "context";
 import { Comic } from "dh-marvel/features/Types/comic.types"
-import { Card, Delivery, Register, State, ComicInfo } from "dh-marvel/features/Types/state.types"
+import { Card, Delivery, Register, Order, ComicInfo } from "dh-marvel/features/Types/state.types"
 
 export const types = {
     ADD_COMIC: 'ADD_COMIC',
@@ -14,33 +15,35 @@ export type Action =
   |  { type: 'SUBMIT_DELIVERY', payload: Delivery }
   |  { type: 'SUBMIT_CARD', payload: Card };
 
-export const initialState: State = {
-    comic: {
-        img: "",
-        price: 0,
-        title: "",
-    },
-    register: {
-        usuario: "",
-        apellido: "",
-        email: "",
-    },
-    delivery: {
-        direccion: "Siempre Viva 123",
-        departamento: "",
-        ciudad: "",
-        provincia: "",
-        codigoPostal: "",
-    },
-    card: {
-        nroTarjeta: "",
-        nombreTarjeta: "",
-        fechaExp: "",
-        codSeguridad: "",
+export const initialState: OrderState = { 
+    order: {
+        comic: {
+            img: "",
+            price: 0,
+            title: "",
+        },
+        register: {
+            usuario: "",
+            apellido: "",
+            email: "",
+        },
+        delivery: {
+            direccion: "Siempre Viva 123",
+            departamento: "",
+            ciudad: "",
+            provincia: "",
+            codigoPostal: "",
+        },
+        card: {
+            nroTarjeta: "",
+            nombreTarjeta: "",
+            fechaExp: "",
+            codSeguridad: "",
+        }
     }
 }
 
-export const reducer = (state: State, action: Action) => {
+export const reducer = (state: OrderState, action: Action) => {
     switch (action.type) {
         case 'ADD_COMIC':
             return {...state, comic: action.payload}
